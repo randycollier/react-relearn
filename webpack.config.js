022eb2ce945a15d857a2cd7e46c4791eb33d80e2
Randy Collier <randycollier.tx@gmail.com>
2018-02-22 13:04:51 -0600

    Ui framework (#3)
    * add bootstrap
    
    * add human route and env vars
    
    * redux and human app
    
    * add human api route
    
    * add db config
    
    * point to view with basedir
    
    * db
    
    * Update .gitignore
    
    * add server_lib
    
    * debug
    
    * debug
    
    * fix route
    
    * log
    
    * add the path module
    
    * update path
    
    * fix
    
    * test
    
    * add logging
    
    * add router
    
    * add redux
    
    * clean up
    
    * clean up
    
    * update env
    
    * update env
    
    * update env
    
    * update env
    
    * update env
    
    * update env
    
    * add front page
    
    * fix chrome redux devtools
    
    * add more to the home page
    
    * add resume
    
    * update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const webpack = require('webpack')
-
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+
 
 const config = {
   resolve: {
     modules: [
       path.resolve('./lib'),
-      path.resolve('./node_modules')
+      path.resolve('./node_modules'),
     ]
   },
   entry: {
@@ -14,9 +17,18 @@ const config = {
       'babel-polyfill',
       'react',
       'react-dom',
+      'react-router-dom',
       'prop-types',
-      'axios'
+      'axios',
+      'redux-thunk',
+      'styled-components'
+
+
+
+
+
     ],
+    style: ['./lib/style/app.scss'],
     app: ['./lib/renderers/dom.js']
   },
   output: {
@@ -33,11 +45,22 @@ const config = {
           presets: ['react', 'env', 'stage-2']
         }
       }
+    },
+    {
+      test:/\.(s*)css$/,
+      use: ExtractTextPlugin.extract({
+        fallback:'style-loader',
+        use:['css-loader','sass-loader'],
+      })
     }]
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      disable: process.env.NODE_ENV === 'development'
     })
   ]
 };
